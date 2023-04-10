3aa87c0d09021a3c683f5e74f6e06c5a8d05df06
Toto Budi Trapsilo <toto2441999@gmail.com>
2023-04-10 09:32:29 +0700

    feat: add expenses dynamically

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,89 @@
+import "./ExpenseForm.css";
+import { useState } from "react";
+
+const ExpenseForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  /**
+   * another approach is using a single state
+   * const [userInput, setUserInput]= useState({
+   *    enteredTitle: "",
+   *    enteredAmount: "",
+   *   enteredDate: ""
+   * })
+   * and add this in the onChange handler
+   * note that we can not just use setUserinput({..prevUserinput, enteredTitle: event.target.value}
+   * since React does not change state right away, approach below will make sure that we will always have the latest value of the state and use it to update the state
+   *  setUserInput((prevUserInput)=>{
+   *    return {...prevUserInput, enteredTitle: event.target.value}
+   * })
+   */
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    /**
+     * by default, onSubmit reload the page, and we do not want that
+     */
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    props.onSaveExpenseData(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          ></input>
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+            value={enteredAmount}
+          ></input>
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2023-01-01"
+            max="2024-12-31"
+            onChange={dateChangeHandler}
+            value={enteredDate}
+          ></input>
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
